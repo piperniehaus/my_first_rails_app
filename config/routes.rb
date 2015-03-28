c3a2c2993c67152d77c79654e9c214cbd30212e2
Piper Niehaus <piper.niehaus@gmail.com>
2015-03-27 18:16:19 -0600

    Created pages * Hello * Introductions * Current time
    [#91316082]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'hello' => 'welcome#hello'
   get 'hello/:name' => 'welcome#hello'
+  get 'introduce/:name1/and/:name2' => 'introductions#index'
+  get 'time/now' => 'time#now'
 end
